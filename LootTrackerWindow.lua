fee12808b100ef9e643e49d8a9e59b5f14020632
Gustavo Michels <gustavomichels@icloud.com>
2026-08-12 13:39:13 -0500

    per-char and group/self loot

--- LootTrackerWindow.lua
+++ LootTrackerWindow.lua
@@ -4,40 +4,106 @@ import "Turbine.UI.Lotro";
 
 LootTrackerWindow = class(Turbine.UI.Lotro.Window);
 
-function LootTrackerWindow:Constructor()
+function LootTrackerWindow:Constructor(characterName)
 	Turbine.UI.Lotro.Window.Constructor(self);
 
-    self.list = {};
+    self.history = {};
+    self.characterName = characterName or "Unknown";
+    self.activeTab = "Group";
 
     self:SetSize(500, 420);
     self:SetText("LootTracker");
+
+    self.tabGroup = Turbine.UI.Lotro.Button();
+    self.tabGroup:SetParent(self);
+    self.tabGroup:SetPosition(18, 42);
+    self.tabGroup:SetSize(90, 28);
+    self.tabGroup:SetText("Group");
+    self.tabGroup.Click = function()
+        self:SetActiveTab("Group");
+    end
+
+    self.tabMine = Turbine.UI.Lotro.Button();
+    self.tabMine:SetParent(self);
+    self.tabMine:SetPosition(118, 42);
+    self.tabMine:SetSize(90, 28);
+    self.tabMine:SetText("Mine");
+    self.tabMine.Click = function()
+        self:SetActiveTab("Mine");
+    end
+
+    self.clearButton = Turbine.UI.Lotro.Button();
+    self.clearButton:SetParent(self);
+    self.clearButton:SetPosition(400, 42);
+    self.clearButton:SetSize(80, 28);
+    self.clearButton:SetText("Clear");
+    self.clearButton.Click = function()
+        self.history = {};
+        _G.lootTrackerHistory = self.history;
+        self:ClearList();
+    end
 
     self.verticalScrollbar = Turbine.UI.Lotro.ScrollBar();
     self.verticalScrollbar:SetOrientation(Turbine.UI.Orientation.Vertical);
     self.verticalScrollbar:SetParent(self);
     self.verticalScrollbar:SetZOrder(1);
-    self.verticalScrollbar:SetPosition(500 - 20, 40);
-    self.verticalScrollbar:SetSize(10, 420 - 60);
+    self.verticalScrollbar:SetPosition(500 - 20, 78);
+    self.verticalScrollbar:SetSize(10, 420 - 98);
 
     self.list = Turbine.UI.ListBox();
     self.list:SetParent(self);
-    self.list:SetPosition(20, 40);
-    self.list:SetSize(500 - 40, 420 - 60);
+    self.list:SetPosition(20, 78);
+    self.list:SetSize(500 - 40, 420 - 98);
     self.list:SetVerticalScrollBar(self.verticalScrollbar);
+
+    self:SetActiveTab(self.activeTab);
 end
 
-function LootTrackerWindow:AddItem(data)
+function LootTrackerWindow:SetCharacterName(name)
+    self.characterName = name or self.characterName;
+    self:SetText("LootTracker: " .. self.characterName);
+    self:Refresh();
+end
+
+function LootTrackerWindow:SetActiveTab(tabName)
+    self.activeTab = tabName;
+    self:Refresh();
+end
+
+function LootTrackerWindow:AddItemToList(data)
     local item = LootTrackerItem(data);
     item:SetParent(self);
     self.list:InsertItem(-1, item);
 end
 
-function LootTrackerWindow:LoadData(dataList)
+function LootTrackerWindow:ClearList()
     while self.list:GetItemCount() > 0 do
         self.list:RemoveItemAt(1);
-	end
-
-    for _, data in pairs(dataList) do
-        self:AddItem(data);
     end
+end
+
+function LootTrackerWindow:Refresh()
+    local displayData = {};
+    if self.activeTab == "Group" then
+        for _, data in pairs(self.history) do
+            if data.user ~= self.characterName then
+                table.insert(displayData, data);
+            end
+        end
+    else
+        for _, data in pairs(self.history) do
+            if data.user == self.characterName then
+                table.insert(displayData, data);
+            end
+        end
+    end
+    self:ClearList();
+    for _, data in pairs(displayData) do
+        self:AddItemToList(data);
+    end
+end
+
+function LootTrackerWindow:LoadData(dataList)
+    self.history = dataList or {};
+    self:Refresh();
 end
--- Main.lua
+++ Main.lua
@@ -1,22 +1,46 @@
 import "Turbine";
 import "LootTracker";
 
+local function GetCharacterName()
+    local player = Turbine.Gameplay.LocalPlayer.GetInstance();
+    return player:GetName();
+end
+
 local lootWindow = LootTrackerWindow();
 
+-- cache the current character name so we don't call the API on every chat message
+local currentCharacterName = nil;
+
 function HandleReceivedMessage(sender, args)
-    local user = args.Message:match('(.*) has acquired');
+    local characterName = currentCharacterName or GetCharacterName();
+    local message = args.Message or "";
+    local user = message:match('(.+) has acquired');
+    if not user or user == "" then
+        if message:match('^You have acquired') then
+            user = characterName;
+        end
+    end
+    if not user or user == "" then
+        user = nil;
+    end
+
     if (args.ChatType == Turbine.ChatType.FellowLoot or user) then
-        local idHex = args.Message:match('has acquired <Examine:IIDDID:.*:(.*)>%b[]<\\Examine>');
-        local infoStr = args.Message:match('has acquired <ExamineItemInstance:ItemInfo:(.*)>%b[]<\\ExamineItemInstance>');
+        -- Turbine.Shell.WriteLine("LootTracker received: " .. message);
+        local idHex = message:match('<Examine:IIDDID:.*:(.*)>%b[]<\\Examine>');
+        local infoStr = message:match('<ExamineItemInstance:ItemInfo:(.*)>%b[]<\\ExamineItemInstance>');
         local id = (idHex and idHex) or (infoStr and "0x" .. GetHex(ItemLinkDecode.DecodeLinkData(infoStr, false).itemGID));
-        local time = Turbine.Engine.GetLocalTime();
-        local data = {
-            id = id,
-            user = user,
-            time = time,
-        };
-        table.insert(_G.lootTrackerHistory, -1, data);
-        lootWindow:AddItem(data);
+
+        if id then
+            -- Turbine.Shell.WriteLine("LootTracker: adding to history id=" .. id);
+            local time = Turbine.Engine.GetLocalTime();
+            local data = {
+                id = id,
+                user = user or characterName or "Unknown",
+                time = time,
+            };
+            table.insert(_G.lootTrackerHistory, data);
+            lootWindow:Refresh();
+        end
     end
 end
 
@@ -33,16 +57,33 @@ Turbine.Shell.AddCommand("lt", OpenLootTrackerWindow);
 
 -- load / unload
 Plugins.LootTracker.Load = function ()
-    local filteredLootList = Turbine.PluginData.Load(Turbine.DataScope.Account, "LootTracker");
-    _G.lootTrackerHistory = FilterLootTrackerListPeriod(filteredLootList, ONE_DAY_IN_SECONDS);
+    local characterName = GetCharacterName();
+    currentCharacterName = characterName;
+    local historyKey = "LootTrackerHistory";
+    local windowKey = "LootTrackerWindow";
+    local rawLootList = Turbine.PluginData.Load(Turbine.DataScope.Character, historyKey) or {};
+    local windowState = Turbine.PluginData.Load(Turbine.DataScope.Character, windowKey) or {};
+    _G.lootTrackerHistory = FilterLootTrackerListPeriod(rawLootList, ONE_DAY_IN_SECONDS);
     table.sort(_G.lootTrackerHistory, function (a, b)
         return a.time < b.time;
     end);
-    lootWindow:SetVisible(true);
+    if windowState.left and windowState.top then
+        lootWindow:SetPosition(windowState.left, windowState.top);
+    end
+    if characterName then
+        lootWindow:SetCharacterName(characterName);
+    end
     lootWindow:LoadData(_G.lootTrackerHistory);
 end
 
 Plugins.LootTracker.Unload = function ()
-    local filteredLootList = FilterLootTrackerListPeriod(_G.lootTrackerHistory, ONE_DAY_IN_SECONDS);
-    Turbine.PluginData.Save(Turbine.DataScope.Account, "LootTracker", filteredLootList);
+    local historyKey = "LootTrackerHistory";
+    local windowKey = "LootTrackerWindow";
+    local filteredLootList = FilterLootTrackerListPeriod(_G.lootTrackerHistory or {}, ONE_DAY_IN_SECONDS);
+    Turbine.PluginData.Save(Turbine.DataScope.Character, historyKey, filteredLootList);
+    Turbine.PluginData.Save(Turbine.DataScope.Character, windowKey, {
+        left = lootWindow:GetLeft(),
+        top = lootWindow:GetTop(),
+    });
+    currentCharacterName = nil;
 end
